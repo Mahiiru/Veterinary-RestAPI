a1f59b9ca64ed049817c93446de63ad0ce67a7ac
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-02-14 18:53:48 -0300

    Product Controller and Service implemented.

--- src/main/java/com/mahiiru/veterinaryrestapi/controllers/ProductController.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/controllers/ProductController.java
@@ -1,10 +1,33 @@
 package com.mahiiru.veterinaryrestapi.controllers;
 
 
+import com.mahiiru.veterinaryrestapi.models.Product;
+import com.mahiiru.veterinaryrestapi.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return ResponseEntity.ok(service.getAllProducts());
+    }
+
+    @GetMapping
+    @RequestMapping
+    public ResponseEntity<Optional<Product>> getProductById(@PathVariable Long id){
+        return ResponseEntity.ok(service.getProductById(id));
+    }
 }
--- src/main/java/com/mahiiru/veterinaryrestapi/services/ProductService.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/services/ProductService.java
@@ -1,8 +1,24 @@
 package com.mahiiru.veterinaryrestapi.services;
 
 
+import com.mahiiru.veterinaryrestapi.models.Product;
+import com.mahiiru.veterinaryrestapi.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return repository.findById(id);
+    }
 }
