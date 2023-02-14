3af32318aeae2a861ab9deeb4d394821c7ef794f
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-02-14 18:44:59 -0300

    Order Controller and Service implemented.

--- src/main/java/com/mahiiru/veterinaryrestapi/controllers/OrderController.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/controllers/OrderController.java
@@ -1,10 +1,33 @@
 package com.mahiiru.veterinaryrestapi.controllers;
 
 
+import com.mahiiru.veterinaryrestapi.models.Order;
+import com.mahiiru.veterinaryrestapi.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrderController {
+
+    @Autowired
+    private OrderService service;
+
+    @GetMapping
+    public ResponseEntity<List<Order>> getAllOrders(){
+        return ResponseEntity.ok(service.getAllOrders());
+    }
+
+    @GetMapping
+    @RequestMapping("/{id}")
+    public ResponseEntity<Optional<Order>> getOrderById(@PathVariable Long id){
+        return ResponseEntity.ok(service.getOrderById(id));
+    }
 }
--- src/main/java/com/mahiiru/veterinaryrestapi/services/OrderService.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/services/OrderService.java
@@ -1,8 +1,25 @@
 package com.mahiiru.veterinaryrestapi.services;
 
 
+import com.mahiiru.veterinaryrestapi.models.Order;
+import com.mahiiru.veterinaryrestapi.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> getAllOrders() {
+        return repository.findAll();
+    }
+
+    public Optional<Order> getOrderById(Long id) {
+        return repository.findById(id);
+    }
 }
