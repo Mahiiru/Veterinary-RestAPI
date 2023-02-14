1a2a8bce5635fcdbb52ec3a8a639defd4532fd7e
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-02-14 15:51:33 -0300

    Spring boot project created, Maven dependencies added, application properties configuration added.

---
+++ src/main/java/com/mahiiru/veterinaryrestapi/VeterinaryRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.mahiiru.veterinaryrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinaryRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinaryRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahiiru/veterinaryrestapi/VeterinaryRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahiiru.veterinaryrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
