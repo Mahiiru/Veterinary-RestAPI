1f098ae920f1e1960119d6c36e34c7c37e9529bd
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-02-14 16:50:41 -0300

    UserInfo Controller and Service implemented.

--- src/main/java/com/mahiiru/veterinaryrestapi/controllers/UserInfoController.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/controllers/UserInfoController.java
@@ -1,4 +1,33 @@
 package com.mahiiru.veterinaryrestapi.controllers;
 
+
+import com.mahiiru.veterinaryrestapi.models.UserInfo;
+import com.mahiiru.veterinaryrestapi.services.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/UsersInfo")
 public class UserInfoController {
+
+    @Autowired
+    private UserInfoService service;
+
+    @GetMapping
+    private ResponseEntity<List<UserInfo>> getAllUsersInfo(){
+        return ResponseEntity.ok(service.getAllUsersInfo());
+    }
+
+    @GetMapping
+    @RequestMapping("/{id}")
+    private ResponseEntity<Optional<UserInfo>> getUserInfoById(@PathVariable Long id){
+        return ResponseEntity.ok(service.getUserInfoById(id));
+    }
 }
--- src/main/java/com/mahiiru/veterinaryrestapi/services/UserInfoService.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/services/UserInfoService.java
@@ -1,4 +1,25 @@
 package com.mahiiru.veterinaryrestapi.services;
 
+
+import com.mahiiru.veterinaryrestapi.models.UserInfo;
+import com.mahiiru.veterinaryrestapi.repositories.UserInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserInfoService {
+
+    @Autowired
+    private UserInfoRepository repository;
+
+    public List<UserInfo> getAllUsersInfo() {
+        return repository.findAll();
+    }
+
+    public Optional<UserInfo> getUserInfoById(Long id) {
+        return repository.findById(id);
+    }
 }
