f1e51401f811597a145c68903d6eee018d2c1f8f
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-02-14 16:55:51 -0300

    UserService bug fixed.

--- src/main/java/com/mahiiru/veterinaryrestapi/services/UserService.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/services/UserService.java
@@ -3,10 +3,12 @@ package com.mahiiru.veterinaryrestapi.services;
 import com.mahiiru.veterinaryrestapi.models.User;
 import com.mahiiru.veterinaryrestapi.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserService {
 
     @Autowired
