e5926ca1b05ec48a4841708ce8311fbdd07fdaca
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-02-14 16:37:33 -0300

    Rol Controller and Service implemented.

--- src/main/java/com/mahiiru/veterinaryrestapi/controllers/RolController.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/controllers/RolController.java
@@ -1,4 +1,33 @@
 package com.mahiiru.veterinaryrestapi.controllers;
 
+
+import com.mahiiru.veterinaryrestapi.models.Rol;
+import com.mahiiru.veterinaryrestapi.services.RolService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/roles")
 public class RolController {
+
+    @Autowired
+    private RolService service;
+
+    @GetMapping
+    public ResponseEntity<List<Rol>> getAllRoles(){
+        return ResponseEntity.ok(service.getAllRoles());
+    }
+
+    @GetMapping
+    @RequestMapping("/{id}")
+    public ResponseEntity<Optional<Rol>> getRolById(@PathVariable Long id){
+        return ResponseEntity.ok(service.getRolById(id));
+    }
 }
--- src/main/java/com/mahiiru/veterinaryrestapi/services/RolService.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/services/RolService.java
@@ -1,4 +1,27 @@
 package com.mahiiru.veterinaryrestapi.services;
 
+
+import com.mahiiru.veterinaryrestapi.models.Rol;
+import com.mahiiru.veterinaryrestapi.repositories.RolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class RolService {
+
+    @Autowired
+    private RolRepository repository;
+
+
+    public List<Rol> getAllRoles() {
+        return repository.findAll();
+    }
+
+
+    public Optional<Rol> getRolById(Long id) {
+        return repository.findById(id);
+    }
 }
