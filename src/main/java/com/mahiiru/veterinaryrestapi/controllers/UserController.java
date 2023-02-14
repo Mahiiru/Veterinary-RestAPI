7f3136faf991cbedcead448ab87c9d005ef91f78
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-02-14 16:45:43 -0300

    User Controller and Service implemented.

--- src/main/java/com/mahiiru/veterinaryrestapi/controllers/UserController.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/controllers/UserController.java
@@ -1,4 +1,33 @@
 package com.mahiiru.veterinaryrestapi.controllers;
 
+
+import com.mahiiru.veterinaryrestapi.models.User;
+import com.mahiiru.veterinaryrestapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        return ResponseEntity.ok(service.getAllUsers());
+    }
+
+    @GetMapping
+    @RequestMapping("/{id}")
+    public ResponseEntity<Optional<User>> getUserById(@PathVariable Long id){
+        return ResponseEntity.ok(service.getUserById(id));
+    }
 }
--- src/main/java/com/mahiiru/veterinaryrestapi/services/UserService.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/services/UserService.java
@@ -1,4 +1,21 @@
 package com.mahiiru.veterinaryrestapi.services;
 
+import com.mahiiru.veterinaryrestapi.models.User;
+import com.mahiiru.veterinaryrestapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
 public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+    public List<User> getAllUsers() {
+        return repository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return repository.findById(id);
+    }
 }
