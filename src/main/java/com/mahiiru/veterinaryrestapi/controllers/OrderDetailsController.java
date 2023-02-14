719c5e8da5219df9f1ec5d4129ab1f1a6078394e
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-02-14 18:49:20 -0300

    OrderDetails Controller and Service implemented.

--- src/main/java/com/mahiiru/veterinaryrestapi/controllers/OrderDetailsController.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/controllers/OrderDetailsController.java
@@ -1,10 +1,33 @@
 package com.mahiiru.veterinaryrestapi.controllers;
 
 
+import com.mahiiru.veterinaryrestapi.models.OrderDetails;
+import com.mahiiru.veterinaryrestapi.services.OrderDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/ordersdetails")
 public class OrderDetailsController {
+
+    @Autowired
+    private OrderDetailsService service;
+
+    @GetMapping
+    public ResponseEntity<List<OrderDetails>> getAllOrdersDetails(){
+        return ResponseEntity.ok(service.getAllOrdersDetails());
+    }
+
+    @GetMapping
+    @RequestMapping("/{id}")
+    public ResponseEntity<Optional<OrderDetails>> getOrderDetailsById(@PathVariable Long id){
+        return ResponseEntity.ok(service.getOrderDetailsById(id));
+    }
 }
--- src/main/java/com/mahiiru/veterinaryrestapi/services/OrderDetailsService.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/services/OrderDetailsService.java
@@ -1,8 +1,24 @@
 package com.mahiiru.veterinaryrestapi.services;
 
 
+import com.mahiiru.veterinaryrestapi.models.OrderDetails;
+import com.mahiiru.veterinaryrestapi.repositories.OrderDetailsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderDetailsService {
+
+    @Autowired
+    private OrderDetailsRepository repository;
+    public List<OrderDetails> getAllOrdersDetails() {
+        return  repository.findAll();
+    }
+
+    public Optional<OrderDetails> getOrderDetailsById(Long id) {
+        return repository.findById(id);
+    }
 }
