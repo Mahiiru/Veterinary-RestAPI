015ca951caf72b841a2b7c07381364b0c4b5997a
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-02-15 10:12:25 -0300

    AppointmentDetails Controller and Service implemented.

--- src/main/java/com/mahiiru/veterinaryrestapi/controllers/AppointmentDetailsController.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/controllers/AppointmentDetailsController.java
@@ -1,10 +1,17 @@
 package com.mahiiru.veterinaryrestapi.controllers;
 
 
+import com.mahiiru.veterinaryrestapi.models.AppointmentDetails;
 import com.mahiiru.veterinaryrestapi.services.AppointmentDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/appointmentsDetails")
@@ -12,4 +19,15 @@ public class AppointmentDetailsController {
 
     @Autowired
     private AppointmentDetailsService service;
+
+    @GetMapping
+    public ResponseEntity<List<AppointmentDetails>> getAllAppointmentsDetails(){
+        return ResponseEntity.ok(service.getAllAppointmentsDetails());
+    }
+
+    @GetMapping
+    @RequestMapping("/{id}")
+    public ResponseEntity<Optional<AppointmentDetails>> getAppointmentDetailsById(@PathVariable Long id){
+        return ResponseEntity.ok(service.getAppointmentDetailsById(id));
+    }
 }
--- src/main/java/com/mahiiru/veterinaryrestapi/services/AppointmentDetailsService.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/services/AppointmentDetailsService.java
@@ -1,12 +1,24 @@
 package com.mahiiru.veterinaryrestapi.services;
 
+import com.mahiiru.veterinaryrestapi.models.AppointmentDetails;
 import com.mahiiru.veterinaryrestapi.repositories.AppointmentDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AppointmentDetailsService {
 
     @Autowired
     private AppointmentDetailsRepository repository;
+
+    public List<AppointmentDetails> getAllAppointmentsDetails() {
+        return repository.findAll();
+    }
+
+    public Optional<AppointmentDetails> getAppointmentDetailsById(Long id) {
+        return repository.findById(id);
+    }
 }
