8ba33a785dc97979bd40498d8fbae80161fb0b3d
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-02-15 09:37:15 -0300

    VeterinaryService  Controller and Service implemented.

--- src/main/java/com/mahiiru/veterinaryrestapi/controllers/VeterinaryServiceController.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/controllers/VeterinaryServiceController.java
@@ -1,10 +1,17 @@
 package com.mahiiru.veterinaryrestapi.controllers;
 
 
+import com.mahiiru.veterinaryrestapi.models.VeterinaryService;
 import com.mahiiru.veterinaryrestapi.services.VeterinaryServiceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/veterinaryServices")
@@ -12,4 +19,15 @@ public class VeterinaryServiceController {
 
     @Autowired
     private VeterinaryServiceService service;
+
+    @GetMapping
+    public ResponseEntity<List<VeterinaryService>> getAllVeterinaryServices(){
+        return ResponseEntity.ok(service.getAllVeterinaryServices());
+    }
+
+    @GetMapping
+    @RequestMapping("/{id}")
+    public ResponseEntity<Optional<VeterinaryService>> getVeterinaryServiceById(@PathVariable Long id){
+        return  ResponseEntity.ok(service.getVeterinaryServiceById(id));
+    }
 }
--- src/main/java/com/mahiiru/veterinaryrestapi/services/VeterinaryServiceService.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/services/VeterinaryServiceService.java
@@ -1,13 +1,25 @@
 package com.mahiiru.veterinaryrestapi.services;
 
 
+import com.mahiiru.veterinaryrestapi.models.VeterinaryService;
 import com.mahiiru.veterinaryrestapi.repositories.VeterinaryServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VeterinaryServiceService {
 
     @Autowired
     private VeterinaryServiceRepository repository;
+
+    public List<VeterinaryService> getAllVeterinaryServices() {
+        return repository.findAll();
+    }
+
+    public Optional<VeterinaryService> getVeterinaryServiceById(Long id) {
+        return repository.findById(id);
+    }
 }
