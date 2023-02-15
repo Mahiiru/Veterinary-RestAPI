098045e508a4c6545cfcbd6e3553d24f7300bf51
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-02-15 09:19:38 -0300

    Appointments and VeterinaryService Models implemented.

---
+++ src/main/java/com/mahiiru/veterinaryrestapi/models/Appointments.java
@@ -0,0 +1,31 @@
+package com.mahiiru.veterinaryrestapi.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Appointments {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate date;
+    private LocalTime time;
+
+    @ManyToOne
+    @JoinColumn(name = "user_info_id")
+    private UserInfo userInfo;
+
+}
---
+++ src/main/java/com/mahiiru/veterinaryrestapi/models/VeterinaryService.java
@@ -0,0 +1,28 @@
+package com.mahiiru.veterinaryrestapi.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class VeterinaryService {
+
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+}
