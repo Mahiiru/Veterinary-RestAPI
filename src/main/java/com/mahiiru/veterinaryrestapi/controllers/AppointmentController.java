a121f30080a19142f0061e2c637189ac866e04bb
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-02-15 09:33:56 -0300

    Appointment  Controller and Service implemented.

--- src/main/java/com/mahiiru/veterinaryrestapi/controllers/AppointmentController.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/controllers/AppointmentController.java
@@ -1,10 +1,17 @@
 package com.mahiiru.veterinaryrestapi.controllers;
 
 
+import com.mahiiru.veterinaryrestapi.models.Appointment;
 import com.mahiiru.veterinaryrestapi.services.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/appointments")
@@ -12,4 +19,15 @@ public class AppointmentController {
 
     @Autowired
     private AppointmentService service;
+
+    @GetMapping
+    public ResponseEntity<List<Appointment>> getAllAppointments(){
+        return ResponseEntity.ok(service.getAllAppointments());
+    }
+
+    @GetMapping
+    @RequestMapping("/{id}")
+    public ResponseEntity<Optional<Appointment>> getAppointmentById(@PathVariable Long id){
+        return ResponseEntity.ok(service.getAppointmentById(id));
+    }
 }
--- src/main/java/com/mahiiru/veterinaryrestapi/services/AppointmentService.java
+++ src/main/java/com/mahiiru/veterinaryrestapi/services/AppointmentService.java
@@ -1,13 +1,25 @@
 package com.mahiiru.veterinaryrestapi.services;
 
 
+import com.mahiiru.veterinaryrestapi.models.Appointment;
 import com.mahiiru.veterinaryrestapi.repositories.AppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AppointmentService {
 
     @Autowired
     private AppointmentRepository repository;
+
+    public List<Appointment> getAllAppointments() {
+        return repository.findAll();
+    }
+
+    public Optional<Appointment> getAppointmentById(Long id) {
+        return repository.findById(id);
+    }
 }
